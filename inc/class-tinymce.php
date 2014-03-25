068868cc60c634bf855da9b9a4180521f236982a
bueltge <frank@bueltge.de>
2014-03-25 21:33:08 +0100

    add phpdoc

--- inc/class-tinymce.php
+++ inc/class-tinymce.php
@@ -2,7 +2,7 @@
 /**
  * AddQuicktag - to TinyMCE Editor
  * 
- * @license    GPLv3
+ * @license    GPLv2
  * @package    AddQuicktag
  * @subpackage AddQuicktag 2 TinyMce
  * @author     Frank Bueltge <frank@bueltge.de>
@@ -13,6 +13,10 @@ if ( ! function_exists( 'add_action' ) ) {
 	exit;
 }
 
+/**
+ * Class Add_Quicktag_2_TinyMce
+ * Add teh list box to the tinymce
+ */
 class Add_Quicktag_2_TinyMce extends Add_Quicktag {
 	
 	static private $option_string = 'rmnlQuicktagSettings_tmce';
@@ -35,6 +39,25 @@ class Add_Quicktag_2_TinyMce extends Add_Quicktag {
 	}
 
 	/**
+	 * Add the script url to plugins of TinyMCE
+	 *
+	 * @param $plugins
+	 * @return array
+	 */
+	public function add_externel_buttons( $plugins ) {
+
+		if ( FALSE == is_array( $plugins ) )
+			$plugins = array();
+
+		$suffix = defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ? '.dev' : '';
+
+		$url = plugins_url( '/tinymce/editor_plugin' . $suffix . '.js', __FILE__ );
+		$plugins = array_merge( $plugins, array( self::$option_string => $url ) );
+
+		return $plugins;
+	}
+
+	/**
 	 * Constructor, init on defined hooks of WP and include second class
 	 *
 	 * @access  public
@@ -43,30 +66,24 @@ class Add_Quicktag_2_TinyMce extends Add_Quicktag {
 	 * @return \Add_Quicktag_2_TinyMce
 	 */
 	private function __construct() {
-		
+
 		add_filter( 'mce_external_plugins', array( $this, 'add_externel_buttons' ) );
 		add_filter( 'mce_buttons_2',        array( $this, 'extend_editor_buttons' ), 10, 2 );
 	}
-	
-	public function add_externel_buttons( $plugins ) {
-		
-		if ( FALSE == is_array( $plugins ) )
-			$plugins = array();
-		
-		$suffix = defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ? '.dev' : '';
-		
-		$url = plugins_url( '/tinymce/editor_plugin' . $suffix . '.js', __FILE__ );
-		$plugins = array_merge( $plugins, array( self::$option_string => $url ) );
-		
-		return $plugins;
-	}
-	
+
+	/**
+	 * Add key for address the button via script
+	 *
+	 * @param bool $editor_id
+	 * @return array
+	 * @param $buttons
+	 */
 	public function extend_editor_buttons( $buttons, $editor_id = FALSE ) {
-		
+
 		$buttons = array_merge( array( self::$option_string ), $buttons );
-		
+
 		return $buttons;
 	}
-	
+
 } // end class
 $add_quicktag_2_tinymce = Add_Quicktag_2_TinyMce::get_object();
