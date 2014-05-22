09c45b80209194767a09ec7e4cb0753ce68563d9
bueltge <frank@bueltge.de>
2014-05-22 12:20:25 +0200

    fix for remove qt on save post

--- addquicktag.php
+++ addquicktag.php
@@ -139,7 +139,7 @@ class Add_Quicktag {
 		// Include solution for TinyMCE
 		require_once dirname( __FILE__ ) . DIRECTORY_SEPARATOR . 'inc/class-tinymce.php';
 
-		foreach( $this->get_admin_pages_for_js() as $page ) {
+		foreach ( $this->get_admin_pages_for_js() as $page ) {
 			add_action( 'admin_print_scripts-' . $page, array( $this, 'get_json' ) );
 			add_action( 'admin_print_scripts-' . $page, array( $this, 'admin_enqueue_scripts' ) );
 		}
@@ -158,37 +158,37 @@ class Add_Quicktag {
 	 */
 	public function remove_quicktags( $qtInit ) {
 
-		if ( empty( $qtInit['buttons'] ) ) {
-			$qtInit['buttons'] = '';
+		// No core buttons, not necessary to filter
+		if ( empty( $qtInit[ 'buttons' ] ) ) {
+			return $qtInit;
 		}
 
 		if ( is_multisite() && is_plugin_active_for_network( self::$plugin ) ) {
-			wp_nonce_field( self::$nonce_string );
 			$options = get_site_option( self::$option_string );
 		} else {
-			settings_fields( self::$option_string . '_group' );
 			$options = get_option( self::$option_string );
 		}
 
-		if ( empty( $options['core_buttons'] ) ) {
-			$options['core_buttons'] = array();
+		// No settings, not necessary to filter
+		if ( empty( $options[ 'core_buttons' ] ) ) {
+			return $qtInit;
 		}
 
 		// get only the keys
-		$remove_these = array_keys( $options['core_buttons'] );
+		$remove_these = array_keys( $options[ 'core_buttons' ] );
 
 		// Convert string to array
-		$buttons = explode( ',', $qtInit['buttons'] );
+		$buttons = explode( ',', $qtInit[ 'buttons' ] );
 		// Loop over items to remove and unset them from the buttons
-		for( $i = 0; $i < count( $remove_these ); $i ++ ) {
+		for ( $i = 0; $i < count( $remove_these ); $i ++ ) {
 			if ( FALSE !== ( $key = array_search( $remove_these[ $i ], $buttons ) ) ) {
 				unset( $buttons[ $key ] );
 			}
 		}
 
 		// Convert new buttons array back into a comma-separated string
-		$qtInit['buttons'] = implode( ',', $buttons );
-		$qtInit['buttons'] = apply_filters( 'addquicktag_remove_buttons', $qtInit['buttons'] );
+		$qtInit[ 'buttons' ] = implode( ',', $buttons );
+		$qtInit[ 'buttons' ] = apply_filters( 'addquicktag_remove_buttons', $qtInit[ 'buttons' ] );
 
 		return $qtInit;
 	}
@@ -229,12 +229,12 @@ class Add_Quicktag {
 			$options = get_option( self::$option_string );
 		}
 
-		if ( empty( $options['buttons'] ) ) {
-			$options['buttons'] = '';
+		if ( empty( $options[ 'buttons' ] ) ) {
+			$options[ 'buttons' ] = '';
 		}
 
 		// allow change or enhance buttons array
-		$options['buttons'] = apply_filters( 'addquicktag_buttons', $options['buttons'] );
+		$options[ 'buttons' ] = apply_filters( 'addquicktag_buttons', $options[ 'buttons' ] );
 		// hook for filter options
 		$options = apply_filters( 'addquicktag_options', $options );
 
@@ -242,17 +242,17 @@ class Add_Quicktag {
 			return NULL;
 		}
 
-		if ( 1 < count( $options['buttons'] ) ) {
+		if ( 1 < count( $options[ 'buttons' ] ) ) {
 			// sort array by order value
 			$tmp = array();
-			foreach( $options['buttons'] as $order ) {
-				if ( isset( $order['order'] ) ) {
-					$tmp[] = $order['order'];
+			foreach ( $options[ 'buttons' ] as $order ) {
+				if ( isset( $order[ 'order' ] ) ) {
+					$tmp[ ] = $order[ 'order' ];
 				} else {
-					$tmp[] = 0;
+					$tmp[ ] = 0;
 				}
 			}
-			array_multisort( $tmp, SORT_ASC, $options['buttons'] );
+			array_multisort( $tmp, SORT_ASC, $options[ 'buttons' ] );
 		}
 
 		?>
@@ -288,7 +288,7 @@ class Add_Quicktag {
 
 		$suffix = defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ? '.dev' : '';
 
-		if ( version_compare( $GLOBALS['wp_version'], '3.3alpha', '>=' ) ) {
+		if ( version_compare( $GLOBALS[ 'wp_version' ], '3.3alpha', '>=' ) ) {
 			wp_enqueue_script(
 				self::get_textdomain() . '_script',
 				plugins_url( '/js/add-quicktags' . $suffix . '.js', __FILE__ ),
