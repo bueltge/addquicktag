f406b44ca76f990a7282ee3380a64c27374bb2da
bueltge <frank@bueltge.de>
2014-05-22 12:24:50 +0200

    minify the qt js

--- js/add-quicktags.js
+++ js/add-quicktags.js
@@ -3,9 +3,8 @@
  * 
  * @package  AddQuicktag Plugin
  * @author   Frank Bueltge <frank@bueltge.de>
- * @version  07/11/2012
+ * @version  05/22/2014
  * @since    2.0.0
  */
 
-jQuery(document).ready(function($){if(typeof addquicktag_tags=="undefined")return;if(typeof addquicktag_post_type=="undefined")return;if(typeof addquicktag_pt_for_js=="undefined")return;var tags=addquicktag_tags["buttons"];if(typeof tags=="undefined")return;if($.inArray("addquicktag_post_type",addquicktag_pt_for_js))for(var i=0;i<tags.length;i++)if(1===parseInt(tags[i][addquicktag_post_type])){if(typeof tags[i].title=="undefined")tags[i].title=" ";if(typeof tags[i].end=="undefined")tags[i].end="";
-if(typeof tags[i].access=="undefined")tags[i].access="";QTags.addButton(tags[i].text.toLowerCase(),tags[i].text,tags[i].start,tags[i].end,tags[i].access,tags[i].title);}});
+jQuery(document).ready(function(t){if("undefined"!=typeof addquicktag_tags&&"undefined"!=typeof addquicktag_post_type&&"undefined"!=typeof addquicktag_pt_for_js){var e=addquicktag_tags.buttons;if("undefined"!=typeof e&&t.inArray("addquicktag_post_type",addquicktag_pt_for_js))for(var d=0;d<e.length;d++)1===parseInt(e[d][addquicktag_post_type])&&("undefined"==typeof e[d].title&&(e[d].title=" "),"undefined"==typeof e[d].end&&(e[d].end=""),"undefined"==typeof e[d].access&&(e[d].access=""),QTags.addButton(e[d].text.toLowerCase(),e[d].text,e[d].start,e[d].end,e[d].access,e[d].title))}});
