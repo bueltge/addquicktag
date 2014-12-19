053a593298562e3b783a7ca857fb3396e302b6ab
Frank Bültge <frank@bueltge.de>
2014-12-19 09:14:55 +0100

    Normalize

--- inc/tinymce/langs/de.js
+++
@@ -1,5 +0,0 @@
-tinyMCE.addI18n('de.aqtwe',{
-aqtwe_select_header : 'weitere Formate',
-aqtwe_select_error : 'keine Quicktags erkannt'
-});
-
--- inc/tinymce/langs/en.js
+++
@@ -1,5 +0,0 @@
-tinyMCE.addI18n('en.aqtwe',{
-aqtwe_select_header : 'further styles',
-aqtwe_select_error : 'no Quicktags found'
-});
-
